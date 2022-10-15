b29df78ef1cc824dd70bca821f1b4f438bbcf842
Krzysiek <krzysztofkulpacb@gmail.com>
2022-10-15 17:05:09 +0200

    start work on security 2

--- src/main/java/com/kkulpa/moviesservice/security/ApplicationSecurityConfig.java
+++ src/main/java/com/kkulpa/moviesservice/security/ApplicationSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.kkulpa.moviesservice.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -26,5 +31,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                     .httpBasic();
         }
 
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails testUser = User.builder()
+                                    .username("tu")
+                                    .password("123")
+                                    .roles("USER")
+                                    .build();
 
+        return new InMemoryUserDetailsManager(testUser);
+    }
 }
